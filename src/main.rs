16bd13a85bf4ac3aa8bab7fb665e38ad738ff57a
max <maxleriche.60@gmail.com>
2024-02-15 23:25:58 +0000

    feat: init interface cli

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,14 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(name = "DevCli", about = "A simple cli to ease the dev process with EclipseChe/Devspaces", long_about = None, version)]
 struct Cli {
-    // Optional name to operate on
+    /// Optional name to operate on
     name: Option<String>,
 
-    // Sets a custom config file
+    /// Sets a custom config file
     #[arg(short, long, value_name = "FILE")]
     config: Option<PathBuf>,
 
-    // Turn debugging information on
+    /// Turn debugging information on
     #[arg(short, long, action = clap::ArgAction::Count)]
     debug: u8,
 
@@ -26,10 +26,47 @@ enum Commands {
         #[arg(short, long)]
         list: bool,
     },
+    Workspaces {
+        #[command(subcommand)]
+        workspace: Option<WorkSpaces>,
+    },
 }
-
+/// Handle the workspaces subcommand
 #[derive(Subcommand)]
-enum WorkSpaces {}
+enum WorkSpaces {
+    /// Get the current workspace (if you are in one, either the one selected)
+    Get {},
+    /// Get a list of all containers in the current workspace/pod
+    GetContainer {},
+    /// List all workspaces
+    List {},
+    /// Set the current workspace
+    Set {},
+    /// Start the current workspace
+    Start {},
+    /// Stop the current workspace
+    Stop {},
+    /// Restart the current workspace
+    Restart {},
+    /// Restart the current workspace from local devfile
+    RestartLocal {},
+    /// Spawn a shell in the selected container
+    Shell {
+        /// The name of the container to spawn the shell in
+        name: String,
+
+        /// The shell to spawn
+        #[arg(short, long, default_value = "bash")]
+        shell: String,
+    },
+    /// Open the selected workspace in vscode
+    OpenVsCode {
+        /// The name of the container to spawn the vscode in
+        name: String,
+    },
+    /// Get the info to open the workspace in Idea
+    InfoIdea {},
+}
 
 fn main() {
     let cli = Cli::parse();
@@ -57,6 +94,7 @@ fn main() {
                 println!("Not printing list");
             }
         }
+        &Some(Commands::Workspaces { .. }) => todo!(),
         None => println!("No subcommand"),
     }
 }
