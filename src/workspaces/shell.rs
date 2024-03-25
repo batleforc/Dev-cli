a36c6edb84a562a0acad7d72bd0f2595cfffbeb5
max <maxleriche.60@gmail.com>
2024-03-25 22:57:25 +0000

    feat: prepare process

--- src/workspaces/shell.rs
+++ src/workspaces/shell.rs
@@ -1,4 +1,6 @@
 use crate::{config::CurrentWorkspace, shell::find_pod_by_ws_name::find_pod_by_ws_name};
+use k8s_openapi::api::core::v1::Pod;
+use kube::api::AttachParams;
 use tracing::event;
 
 #[tracing::instrument(level = "trace")]
@@ -11,14 +13,44 @@ pub async fn spawn_shell(
         Some(iencli) => iencli,
         None => return,
     };
-    let pod = match find_pod_by_ws_name(client, current_workspace.clone()).await {
+    let pod = match find_pod_by_ws_name(client.clone(), current_workspace.clone()).await {
         Some(pod) => pod,
         None => {
             event!(tracing::Level::ERROR, "Pod's not found");
             return;
         }
     };
-    let container_cible = match container_name {
+    let container_cible = match select_pod(container_name, pod.clone()) {
+        Some(c_name) => c_name,
+        None => return,
+    };
+    let pod_api = current_workspace.get_api::<Pod>(client);
+
+    let mut attach_param = AttachParams::interactive_tty();
+    attach_param.container = Some(container_cible);
+
+    let process = match pod_api
+        .exec(&pod.metadata.name.unwrap(), vec![shell], &attach_param)
+        .await
+    {
+        Ok(attached_process) => {
+            event!(tracing::Level::TRACE, "Success createing remote process");
+            attached_process
+        }
+        Err(err) => {
+            event!(
+                tracing::Level::ERROR,
+                ?err,
+                "Error while creating remote process"
+            );
+            return;
+        }
+    };
+}
+
+#[tracing::instrument(level = "trace")]
+fn select_pod(target_name: Option<String>, pod: Pod) -> Option<String> {
+    match target_name {
         Some(container_named) => {
             if !pod
                 .spec
@@ -33,9 +65,9 @@ pub async fn spawn_shell(
                     "Pod does not have container : {}",
                     container_named.to_string()
                 );
-                return;
+                return None;
             }
-            container_named
+            Some(container_named)
         }
         None => match pod.spec.unwrap().containers.first() {
             Some(container) => {
@@ -44,15 +76,15 @@ pub async fn spawn_shell(
                     "Using first container : {}",
                     container.name.to_string()
                 );
-                container.name.to_string()
+                Some(container.name.to_string())
             }
             None => {
                 event!(
                     tracing::Level::ERROR,
                     "No container in the pod ? what did you do?"
                 );
-                return;
+                return None;
             }
         },
-    };
+    }
 }
